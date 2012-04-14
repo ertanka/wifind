bba17af8446fc5fa6a79816327b4d0ab40ca7b2f
ertancan <tosss.beee@gmail.com>
2012-04-14 14:04:22 +0300

    select

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-echo "LAn!!";
+connectToDb();
+$query="SELECT * FROM User";
+$result=mysql_query($query);
+var_dump($result);
+
+function connectToDb(){
+$user="wifind";
+$password="wifi0102";
+$database="wifind"; //Database adõ
+mysql_connect(localhost,$user,$password);
+@mysql_select_db($database) or die( "Unable to select database");
+}
+function disconnectFromDb(){    
+mysql_close();
+}
 ?>
