dc5ccd6691ce89e619c3a6f1f831f080d840dc6d
ertancan <tosss.beee@gmail.com>
2012-04-14 14:18:14 +0300

    deneme

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ mysql_connect(localhost,$user,$password);
 @mysql_select_db($database) or die( "Unable to select database");
 }
 function getUpdates(){
-    $query="SELECT * FROM `Status` JOIN User WHERE Status.user_id=User.id";
+    $query="SELECT * FROM Status JOIN User WHERE Status.user_id=User.id";
     $result=mysql_query($query);
     $rows = array();
     while($r = mysql_fetch_assoc($sth)) {
